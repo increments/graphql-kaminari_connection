dc5a66bf0d46e791ed2af379fd0522f1c86fa2ac
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-09-27 17:44:32 +0900

    Generate codes by template

---
+++ lib/graphql/kaminari_connection.rb
@@ -0,0 +1,7 @@
+require "graphql/kaminari_connection/version"
+
+module Graphql
+  module KaminariConnection
+    # Your code goes here...
+  end
+end
---
+++ lib/graphql/kaminari_connection/version.rb
@@ -0,0 +1,5 @@
+module Graphql
+  module KaminariConnection
+    VERSION = "0.1.0"
+  end
+end
